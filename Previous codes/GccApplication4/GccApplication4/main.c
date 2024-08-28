2d4f881b6d24a9178379d31d65a39969ca8e4898
L.H.Hirusha Maduwantha <maduwanthalhh.20@uom.lk>
2024-08-28 06:00:38 +0530

    Add logic

---
+++ Previous codes/GccApplication4/GccApplication4/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication4.c
+ *
+ * Created: 8/21/2024 1:05:50 PM
+ * Author : Ama
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
